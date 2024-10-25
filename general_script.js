8de0e7f30680f777b9812852f5d386c542a82028
Chua Rui Han <chuaruihan8@gmail.com>
2024-10-25 10:28:08 +0800

    sql database and 1 movie sample

--- general_script.js
+++ general_script.js
@@ -14,6 +14,20 @@ document.addEventListener("DOMContentLoaded", function () {
     requestAnimationFrame(raf);
 });
 
+// Navbar
+document.addEventListener("scroll", function() {
+
+	const navbar = document.getElementsByClassName("navbar-wrapper")[0];
+	const scrollHeight = 20;
+
+	if (window.scrollY > scrollHeight) {
+			navbar.classList.add("scrolled");
+	} else {
+			navbar.classList.remove("scrolled");
+	}
+	
+});
+
 // Cinema seating
 document.addEventListener('DOMContentLoaded', () => {
 	const rows = 7;
