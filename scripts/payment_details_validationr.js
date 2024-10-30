0aa93fd6745176398b89460fc3947b8287e1d1a8
Chua Rui Han <chuaruihan8@gmail.com>
2024-10-30 11:26:30 +0800

    debug in class

--- scripts/payment_details_validationr.js
+++ scripts/payment_details_validationr.js
@@ -6,4 +6,3 @@ var paymentMethodNode = document.getElementById("payment-method")
 nameNode.addEventListener("change", chkName, false);
 emailNode.addEventListener("change", chkEmail, false);
 contactNumberNode.addEventListener("change", chkContactNumber, false);
-paymentMethodNode.addEventListener("change",chkPaymentMethod, false);
