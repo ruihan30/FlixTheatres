3dd2a194a2b9c6fc9dce83bb412bffacc7dfa3c6
lytbryan <bryan230100@hotmail.com>
2024-11-01 23:26:25 +0800

    test and debug confirmation email

--- php_files/email_confirmation.php
+++ php_files/email_confirmation.php
@@ -7,12 +7,11 @@ $contact = $_SESSION['contact']; // contact number
 // movie details
 $time = $_SESSION['time']; // showtime
 $date = $_SESSION['date']; // date of show
-$movie = $_SESSION['movie']; // movie title
+$title = $_SESSION['title']; // movie title
 $cinema = $_SESSION['cinema']; // cinema location
 
 // Retrieve the selected seats from the session
 $selected_seats = $_SESSION['selected_seats']; // user selected seats
-$selected_combos = $_SESSION['selected_combos']; // user selected food combo
 
 // Format the selected seats as a comma-separated string
 $selected_seats_string = implode(', ', $selected_seats); 
@@ -22,11 +21,9 @@ $subject = 'Ordered tickets successfully!'; // email subject
 $message = "
 Dear $name,
 
-Thank you for your purchase of $movie tickets for $time on $date at $cinema.
+Thank you for your purchase of $title tickets for $time on $date at $cinema.
 
 Your seats: $selected_seats_string.
-
-Your combo: $selected_combos.
 
 We look forward to your visit at Flix Theatres. Have a nice day!
 
